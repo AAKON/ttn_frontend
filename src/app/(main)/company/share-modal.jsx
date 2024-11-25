6f510dfd18551614c113d45da2080b73664d12d5
Alamin-Coding <alamin.codelover@gmail.com>
2024-11-26 02:03:44 +0600

    share modal issue fixed

--- src/app/(main)/company/share-modal.jsx
+++ src/app/(main)/company/share-modal.jsx
@@ -1,10 +1,12 @@
 import {
   CopyIcon,
-  Facebook,
+  FacebookFIcon,
   ShareBoldIcon,
   TwitterIcon,
   WhatsAppIcon,
+  RedditIcon,
 } from "@/components/icons";
+import { EnvelopeIcon } from "@/components/icons/envelope";
 import Button from "@/components/ui/button";
 import {
   Dialog,
@@ -25,50 +27,57 @@ const ShareModal = () => {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[464px] p-5 ">
         <DialogHeader>
-          <DialogTitle>Share</DialogTitle>
+          <DialogTitle className="text-xl font-semibold text-gray-900">
+            Share
+          </DialogTitle>
         </DialogHeader>
 
         <div className="grid gap-4 py-4">
           <ul className="flex items-center gap-4">
-            <li className="px-2">
+            <li className="px-2 flex flex-col gap-1 items-center">
               <a
                 href="#"
                 className="size-[56px] rounded-full bg-[#65D072] flex items-center justify-center text-white"
               >
-                <WhatsAppIcon stroke="#ffffff" />
+                <WhatsAppIcon stroke="#ffffff" width={30} height={30} />
               </a>
+              <span className="text-xs text-gray-500">WhatsApp</span>
             </li>
-            <li className="px-2">
+            <li className="px-2 flex flex-col gap-1 items-center">
               <a
                 href="#"
                 className="size-[56px] rounded-full bg-[#425893] flex items-center justify-center text-white"
               >
-                <Facebook stroke="#ffffff" />
+                <FacebookFIcon stroke="#ffffff" />
               </a>
+              <span className="text-xs text-gray-500">Facebook</span>
             </li>
-            <li className="px-2">
+            <li className="px-2 flex flex-col gap-1 items-center">
               <a
                 href="#"
                 className="size-[56px] rounded-full bg-[#425893] flex items-center justify-center text-white"
               >
-                <TwitterIcon stroke="#ffffff" />
+                <TwitterIcon stroke="#ffffff" width={30} height={24} />
               </a>
+              <span className="text-xs text-gray-500">Twitter</span>
             </li>
-            <li className="px-2">
+            <li className="px-2 flex flex-col gap-1 items-center">
               <a
                 href="#"
                 className="size-[56px] rounded-full bg-[#888888] flex items-center justify-center text-white"
               >
-                <Facebook stroke="#ffffff" />
+                <EnvelopeIcon stroke="#ffffff" />
               </a>
+              <span className="text-xs text-gray-500">Email</span>
             </li>
-            <li className="px-2">
+            <li className="px-2 flex flex-col gap-1 items-center">
               <a
                 href="#"
                 className="size-[56px] rounded-full bg-[#FF4500] flex items-center justify-center text-white"
               >
-                <Facebook stroke="#ffffff" />
+                <RedditIcon stroke="#ffffff" />
               </a>
+              <span className="text-xs text-gray-500">Reddit</span>
             </li>
           </ul>
           <div className="relative border border-gray-200 rounded-[8px] grid grid-cols-[1fr_auto] gap-2 h-12">
