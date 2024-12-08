72fe135dbbcdc0574b47f781190dc833a957f01d
Alamin-Coding <alamin.codelover@gmail.com>
2024-12-08 21:51:28 +0600

    added business page

---
+++ src/app/(main)/business/page.jsx
@@ -0,0 +1,16 @@
+import { Section } from "@/components/shared";
+import React from "react";
+
+const Business = () => {
+  return (
+    <Section>
+      <div className="mx-auto text-center">
+        <h3 className="text-gray-900   font-semibold text-3xl sm:text-5xl md:leading-[60px]">
+          Find Your Apparel Needs
+        </h3>
+      </div>
+    </Section>
+  );
+};
+
+export default Business;
