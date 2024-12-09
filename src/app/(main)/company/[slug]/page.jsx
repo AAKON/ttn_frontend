c101bb50484644bbf3faa72c5f4f99cbfd1c3fe7
Jahangir Ahmed <jahangir4csit@gmail.com>
2024-12-09 18:05:44 +0600

    added company details page

--- src/app/(main)/company/[slug]/page.jsx
+++ src/app/(main)/company/[slug]/page.jsx
@@ -2,11 +2,12 @@ import Image from "next/image"
 import React, {Suspense} from "react";
 import ErrorMessage from "@/components/shared/errormessage";
 import {getCompanyBasic} from "@/services/company";
-import BasicCompanyView from "@/app/(main)/company/[slug]/components/basicCompanyView";
 import {Container} from "@/shared";
-import Button from "@/components/ui/button";
-import {EditIcon, ViewAs} from "@/icons";
-import Link from "next/link";
+import Frame from "@/components/company/_frame";
+import AboutCompany from "@/components/company/about-company";
+import ProductShowcase from "@/components/company/product-showcase";
+import CompanyTabs from "@/components/company/company-tabs";
+import ContactWithBusinessOwner from "@/components/company/contact-with-business-owner";
 
 
 
@@ -18,27 +19,32 @@ const CompanyDetails = async ({params: { slug }}) => {
         const basic = await basicPromise;
 
         return (
-            <div className="bg-gray-50">
-                <div className="bg-detailBennar bg-no-repeat bg-center bg-cover lg:h-[440px] h-[42.667vw] w-full"></div>
-                <Container>
-                    <div className="bg-white border border-gray-100 p-8 mb-8 rounded-2xl -mt-[140px]">
-                        <div className="flex justify-end gap-3">
-                            <Button secondary>
-                                <ViewAs stroke="#000000"/>
-                                View as
-                            </Button>
-                            <Button TagName={Link} href={`/myaccount/company/edit/${slug}`}>
-                                <EditIcon stroke="#ffffff"/>
-                                Edit My Profile
-                            </Button>
+            <div className="bg-gray-50 pb-8 md:pb-10 lg:pb-16 xl:pb-20">
+                <div className="relative">
+                    <Frame/>
+                    <Container>
+                        <div
+                            className="grid grid-cols-1 gap-6 lg:grid-cols-[1fr_340px] xl:grid-cols-[1fr_370px] xl:gap-12 relative mt-4 xl:mt-10">
+                            <div>
+                                <div className="p-4 lg:p-6 bg-white rounded-2xl z-[2]">
+                                    {/* AboutCompany part start */}
+                                    <div className="lg:mb-[32px] relative grid grid-cols-1 gap-6 xl:gap-8">
+                                        <AboutCompany/>
+                                        <ProductShowcase/>
+                                    </div>
+                                    {/* AboutCompany part end */}
+                                </div>
+                                <div className="mt-8">
+                                    <CompanyTabs />
+                                </div>
+                            </div>
+
+                            <div>
+                                <ContactWithBusinessOwner />
+                            </div>
                         </div>
-                        <div className="pt-4">
-                            <Suspense fallback={<div>Loading ...</div>}>
-                                <BasicCompanyView basic={basic}/>
-                            </Suspense>
-                        </div>
-                    </div>
-                </Container>
+                    </Container>
+                </div>
             </div>
         )
     } catch (error) {
