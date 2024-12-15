b5dbd48ae8d98ca5bd0979a1880b1d972e92fc37
Alamin-Coding <alamin.codelover@gmail.com>
2024-12-15 23:31:58 +0600

    home page responsive issue fixed

--- src/ui/option.jsx
+++ src/ui/option.jsx
@@ -4,19 +4,25 @@ import image from "@/assets/option.svg";
 const Option = ({ index, item }) => {
   const { title, img } = item;
   return (
-    <div className="flex items-center gap-x-4">
-      <Image
-        src={img ? img : image}
-        alt="image"
-        className={` ${index === 0 ? "w-[64px]" : "w-[56px]"}`}
-      />
-      <h4
+    <div className="Partner_Benefits flex items-center gap-x-4">
+      <div
+        className={`rounded-full overflow-hidden ${
+          index === 0 ? "size-[28px] md:size-[64px]" : "size-[28px] md:size-[56px]"
+        }`}
+      >
+        <Image
+          src={img ? img : image}
+          alt="image"
+          className="size-full rounded-full"
+        />
+      </div>
+      <p
         className={`leading-normal ${
-          index === 0 ? "text-gray-900 text-[30px]" : "text-gray-700 text-2xl"
+          index === 0 ? "text-gray-900 active" : "text-gray-700"
         }`}
       >
         {title}
-      </h4>
+      </p>
     </div>
   );
 };
