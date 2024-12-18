d1f49f98ccb1bdc8c1572a9fb60c77970bf9b934
Alamin-Coding <alamin.codelover@gmail.com>
2024-12-19 02:08:11 +0600

    Link check, profile page both side slide shadow add smoth slide, Home page Marquee slider border add and design issue fixed

--- src/app/(main)/myaccount/profile/components/my-company.jsx
+++ src/app/(main)/myaccount/profile/components/my-company.jsx
@@ -7,30 +7,29 @@ import "@splidejs/react-splide/css";
 
 const options = {
   type: "loop",
-  perPage: 3.3,
+  perPage: 2 ,
   perMove: 1,
-  gap: "1rem",
+  drag   : 'free',
   pagination: false,
   arrows: false,
-  padding: "4rem",
+  padding: "140px",
   breakpoints: {
-    1024: {
-      perPage: 3.3,
-      padding: "3rem",
+    768: {
+      perPage: 2,
+      padding: "5rem",
     },
     768: {
-      perPage: 2.5,
-      padding: "1rem",
+      perPage: 1,
+      padding: "3rem",
     },
     640: {
-      perPage: 1.5,
+      perPage: 1,
       padding: "0",
     },
   },
 };
 
-const MyCompany = ({companies}) => {
-
+const MyCompany = ({ companies }) => {
   let splideRef = null;
   const handlePrev = () => {
     if (splideRef) splideRef.go("<");
@@ -39,7 +38,6 @@ const MyCompany = ({companies}) => {
   const handleNext = () => {
     if (splideRef) splideRef.go(">");
   };
-
 
   return (
     <div className="bg-white shadow-sm rounded-2xl p-6 border border-gray-100">
@@ -91,17 +89,18 @@ const MyCompany = ({companies}) => {
         </div>
       </div>
       {companies && Array.isArray(companies) && companies.length > 0 && (
-      <Splide
-        className="company-slider"
-        options={options}
-        ref={(splide) => (splideRef = splide)}
-      >
-        {companies?.map((company) => (
-          <SplideSlide key={company.id}>
-            <CompanyCardProfile data={company} />
-          </SplideSlide>
-        ))}
-      </Splide>)}
+        <Splide
+          className="company-slider slider_both_side_shadow"
+          options={options}
+          ref={(splide) => (splideRef = splide)}
+        >
+          {companies?.map((company) => (
+            <SplideSlide key={company.id}>
+              <CompanyCardProfile data={company} />
+            </SplideSlide>
+          ))}
+        </Splide>
+      )}
       {/* <CompanyCardProfile key={company.id} data={company} /> */}
     </div>
   );
--- src/components/marquee-sliders/marquee-slider.jsx
+++ src/components/marquee-sliders/marquee-slider.jsx
@@ -1,25 +1,36 @@
-
 import Marquee from "react-fast-marquee";
 import Image from "next/image";
 import Link from "next/link";
 
-const MarqueeSlider = ({slideItems, className="mr-20", ...props}) => {
+const MarqueeSlider = ({ slideItems, className = "mr-20", ...props }) => {
   return (
-      <>
-        <Marquee
-            autoFill={true}
-            gradientColor="white"
-            gradientWidth={200}
-            gradient={true}
-            pauseOnHover={true}
-        >
-          {slideItems?.map((item) => (
-              <Link href={item?.link ? item?.link : '#'} key={item?.id} className={`cursor-pointer ${className}`} {...props}>
-                <Image width={148} height={148} src={item?.image ? item?.image : ''} className="object-contain" alt={"company image"}/>
-              </Link>
-          ))}
-        </Marquee>
-      </>
+    <>
+      <Marquee
+        autoFill={true}
+        gradientColor="white"
+        gradientWidth={200}
+        gradient={true}
+        pauseOnHover={true}
+      >
+        {slideItems?.map((item) => (
+          <Link
+            href={item?.link ? item?.link : "#"}
+            key={item?.id}
+            className={`size-[100px] flex items-center justify-center bg-white border border-gray-300 ${(className =
+              "mr-[85px]")}`}
+            {...props}
+          >
+            <Image
+              width={100}
+              height={100}
+              src={item?.image ? item?.image : ""}
+              className="w-full h-full object-contain"
+              alt={"company image"}
+            />
+          </Link>
+        ))}
+      </Marquee>
+    </>
   );
 };
 
