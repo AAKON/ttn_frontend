b352886d541035efb8b44efe9aa45ae6ff835957
Jahangir Ahmed <jahangir4csit@gmail.com>
2024-12-25 11:56:23 +0600

    company details see more  ...

--- src/components/company/about-company.jsx
+++ src/components/company/about-company.jsx
@@ -1,25 +1,41 @@
-// "use client";
-// import { useState } from 'react';
+"use client";
+import { useState } from 'react';
 import {ChevronDownIcon} from "@/icons";
+import {ChevronDown, ChevronUp} from "lucide-react";
 
 const AboutCompany = ({aboutData}) => {
-
-    // const [Show, setShow] = useState(false);
-
+    const [isExpanded, setIsExpanded] = useState(false);
+    const previewText = aboutData.split('. ').slice(0, 3).join('. ') + '.';
     return (
         <div>
             <h2 className="text-lg font-semibold text-gray-900">About Company</h2>
             <p className="text-base text-gray-800 font-normal mt-4">
                 {/*<span className="max-sm:hidden sm:hidden lg:block">{aboutData}</span>*/}
-                {/*<span className="lg:hidden">{Show ? aboutData : aboutData?.slice(0, 129) + "..."}</span>*/}
-                {aboutData}
+                <span>{isExpanded ? aboutData : aboutData?.slice(0, 300) + "..."}</span>
             </p>
-            {/*<h3 className="text-sm text-brand-700 leading-sm font-semibold lg:mt-[18px] cursor-pointer flex items-end gap-2"*/}
-            {/*    onClick={() => setShow(!Show)}>*/}
-            {/*    Show more <ChevronDownIcon stroke="#C67618" />*/}
-            {/*</h3>*/}
+            <div className="text-sm text-brand-700 leading-sm font-semibold lg:mt-[18px] cursor-pointer flex items-end gap-2"
+                 onClick={() => setIsExpanded(!isExpanded)}>
+                {isExpanded ? (
+                    <>
+                        See Less <ChevronUp className="w-5 h-5 stroke-current" />
+                    </>
+                ) : (
+                    <>
+                        See More <ChevronDown className="w-5 h-5 stroke-current" />
+                    </>
+                )}
+            </div>
         </div>
     )
 }
+
+// Simulating server-side data fetching for SEO
+export const getServerSideProps = async ({aboutData}) => {
+    return {
+        props: {
+            aboutData,
+        },
+    };
+};
 
 export default AboutCompany
