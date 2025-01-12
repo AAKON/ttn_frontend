880473f545b6b10b9bb632620ce240f668e5e415
Jahangir Ahmed <jahangir4csit@gmail.com>
2025-01-12 10:07:43 +0600

    terms api done

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -7,24 +7,20 @@ import GlobalSourcing from "@/components/global-sourcing/global-sourcing";
 import GlobalMarket from "@/components/global-market/global-market";
 import Resources from "@/components/resources/resources";
 import GetInTouch from "@/components/get-in-touch/get-in-touch";
-import React, { Fragment } from "react";
+import React, {Fragment, Suspense} from "react";
 import { Empty, Section } from "@/shared";
-
-// company Slide items
-import compnany1 from "@/assets/company1.jpg";
-import compnany2 from "@/assets/company2.jpg";
-import compnany3 from "@/assets/company3.jpg";
-import compnany4 from "@/assets/company4.jpg";
 import ErrorMessage from "@/components/shared/errormessage";
 import { getHomeDetails } from "@/services/home";
 import { getBlogTTNS } from "@/services/blogs";
 import MarqueeSlide from "@/components/marquee-sliders/marquee-slide";
+import {GlobalSkeleton} from "@/components/shared/skelton/globalSkeleton";
 
 export default async function Home() {
   try {
-    const blogsPromise = getBlogTTNS();
-    const detailsPromise = getHomeDetails();
-    const details = await detailsPromise;
+      const [blogs, details] = await Promise.all([
+          getBlogTTNS(),
+          getHomeDetails()
+      ]);
 
     const recentCompanies = details?.companies || [];
     const partners = details?.partners || [];
@@ -32,32 +28,30 @@ export default async function Home() {
     const locations = details?.locations || [];
     const webAds = details?.webAds || [];
 
-    //console.log(details, "get detailsPromise");
-
     return (
-      <>
-        <Hero categories={categories} locations={locations} />
-        <Counter />
-        <Fragment>
-          {webAds && Array.isArray(webAds) && webAds.length > 0 ? (
-            <SocialSlider webAds={webAds} />
-          ) : (
-            <Empty message="No webAds found." />
-          )}
-        </Fragment>
-        <RecentCompany data={recentCompanies} />
-        <BusinessArea />
-        <Section className="bg-gray-50">
-          <h3 className="pb-8 font-medium text-gray-900 uppercase text-xl text-center">
-            We’ve worked with some great Companies
-          </h3>
-          <MarqueeSlide slideItems={partners} />
-        </Section>
-        <GlobalSourcing />
-        <GlobalMarket />
-        <Resources blogsPromise={blogsPromise} />
-        <GetInTouch />
-      </>
+        <Suspense fallback={<GlobalSkeleton />}>
+            <Hero categories={categories} locations={locations} />
+            <Counter />
+            <Fragment>
+                {webAds && Array.isArray(webAds) && webAds.length > 0 ? (
+                    <SocialSlider webAds={webAds} />
+                ) : (
+                    <Empty message="No webAds found." />
+                )}
+            </Fragment>
+            <RecentCompany data={recentCompanies} />
+            <BusinessArea />
+            <Section className="bg-gray-50">
+                <h3 className="pb-8 font-medium text-gray-900 uppercase text-xl text-center">
+                    We’ve worked with some great Companies
+                </h3>
+                <MarqueeSlide slideItems={partners} />
+            </Section>
+            <GlobalSourcing />
+            <GlobalMarket />
+            <Resources blogsPromise={blogs} />
+            <GetInTouch />
+        </Suspense>
     );
   } catch (error) {
     return <ErrorMessage message={error?.message} />;
